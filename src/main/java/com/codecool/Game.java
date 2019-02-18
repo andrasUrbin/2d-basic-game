e8c7d37d7b110123ba5b2a943c818e2336c78ec9
András Urbin <urbinandras@gmail.com>
2019-02-18 14:51:06 +0100

    Basic structure, Game and Window added

---
+++ src/main/java/com/codecool/Game.java
@@ -0,0 +1,21 @@
+package com.codecool;
+
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable{
+
+    private static final long serialVersionUID = 1550691097823471818L;
+
+
+    public synchronized void start(){
+
+    }
+
+    public void run(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
